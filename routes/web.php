6a736c5f215812e26953785ff06646f44c5f5888
koutarou <matsuikoutarou@kotaroMacBookPro.local>
2020-06-19 16:39:40 +0900

    no message

--- routes/web.php
+++ routes/web.php
@@ -19,60 +19,60 @@ Route::get('/login', function () {
     return view('php/login');
 });
 
-Route::get('/index', function () {
-    return view('php/index');
-});
-
-Route::get('/user', function () {
-    return view('php/user');
-});
-
-Route::get('/attend', function () {
-    return view('php/attend');
-});
-
-Route::get('/classroom', function () {
-    return view('php/classroom');
-});
-
-Route::get('/classroom_edit', function () {
-    return view('php/classroom_edit');
-});
-
-Route::get('/editBooth', function () {
-    return view('php/editBooth');
-});
-
-Route::get('/header', function () {
-    return view('php/header');
-});
-
-Route::get('/index2', function () {
-    return view('php/index2');
-});
-
-Route::get('/layout', function () {
-    return view('php/layout');
-});
-
-Route::get('/registLayout', function () {
-    return view('php/registLayout');
-});
-
-Route::get('/schedule', function () {
-    return view('php/schedule');
-});
-
-Route::get('/registUser', function () {
-    return view('php/registUser');
-});
-
-//Route::get('/user', 'ConnectController@login');
-
-Route::get('/dbtest', function () {
-    return view('php/dbtest');
-});
-
-Route::get('/attend2', function () {
-    return view('php/attend2');
-});
+//Route::get('/index', function () {
+//    return view('php/index');
+//});
+//
+//Route::get('/user', function () {
+//    return view('php/user');
+//});
+//
+//Route::get('/attend', function () {
+//    return view('php/attend');
+//});
+//
+//Route::get('/classroom', function () {
+//    return view('php/classroom');
+//});
+//
+//Route::get('/classroom_edit', function () {
+//    return view('php/classroom_edit');
+//});
+//
+//Route::get('/editBooth', function () {
+//    return view('php/editBooth');
+//});
+//
+//Route::get('/header', function () {
+//    return view('php/header');
+//});
+//
+//Route::get('/index2', function () {
+//    return view('php/index2');
+//});
+//
+//Route::get('/layout', function () {
+//    return view('php/layout');
+//});
+//
+//Route::get('/registLayout', function () {
+//    return view('php/registLayout');
+//});
+//
+//Route::get('/schedule', function () {
+//    return view('php/schedule');
+//});
+//
+//Route::get('/registUser', function () {
+//    return view('php/registUser');
+//});
+//
+////Route::get('/user', 'ConnectController@login');
+//
+//Route::get('/dbtest', function () {
+//    return view('php/dbtest');
+//});
+//
+//Route::get('/attend2', function () {
+//    return view('php/attend2');
+//});
